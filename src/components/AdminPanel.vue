b06dadb1e0f8fa24d87098f987e0baa914254663
Bartosz Arseniuk <bartekarseniuk1701@gmail.com>
2024-06-24 15:18:05 +0200

    Add user registration, edit and delete functionality

--- src/components/AdminPanel.vue
+++ src/components/AdminPanel.vue
@@ -1,82 +1,123 @@
 <template>
-    <v-container>
-      <v-card-title class="title">Panel Administratora</v-card-title>
-      <v-card-title>
-        <v-btn color="grey darken-3" dark small><v-icon>mdi-plus</v-icon>Dodaj użytkownika</v-btn>
-      </v-card-title>
-      <v-card class="table-card">
-        <v-data-table :headers="headers" :items="users" :items-per-page="5" class="table">
-          <template v-slot:body="{ items }">
-            <tbody v-if="items.length > 0">
-              <tr v-for="user in items" :key="user.id">
-                <td>{{ user.id }}</td>
-                <td>{{ user.name }}</td>
-                <td>{{ user.email }}</td>
-                <td>{{ user.created_at }}</td>
-                <td>
-                  <v-icon @click="editUser(user)">mdi-pencil</v-icon>
-                  <v-icon @click="deleteUser(user.id)">mdi-delete</v-icon>
-                </td>
-              </tr>
-            </tbody>
-            <tbody v-else>
-              <tr>
-                <td colspan="5" class="text-center">Aktualnie tabela nie posiada żadnych rekordów.</td>
-              </tr>
-            </tbody>
-          </template>
-        </v-data-table>
-      </v-card>
-    </v-container>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        headers: [
-          { text: 'L.p.', value: 'id' },
-          { text: 'Nazwa', value: 'name' },
-          { text: 'Email', value: 'email' },
-          { text: 'Data dodania', value: 'created_at' },
-          { text: 'Akcje', value: 'actions', sortable: false },
-        ],
-        users: [],
-      };
+  <v-container>
+    <v-card-title class="title">Panel Administratora</v-card-title>
+    <v-card-title>
+      <v-btn color="grey darken-3" dark small @click="openRegisterModal"><v-icon>mdi-plus</v-icon>Dodaj użytkownika</v-btn>
+    </v-card-title>
+    <v-card class="table-card">
+      <v-data-table :headers="headers" :items="users" :items-per-page="5" class="table">
+        <template v-slot:body="{ items }">
+          <tbody v-if="items.length > 0">
+            <tr v-for="(user, index) in items" :key="user.id" :class="{ 'inactive-row': index === 0 && !user.isActive }">
+              <td>{{ user.id }}</td>
+              <td>{{ user.name }}</td>
+              <td>{{ user.email }}</td>
+              <td>{{ user.created_at }}</td>
+              <td v-if="index !== 0 || user.isActive">
+                <v-icon @click="editUser(user)">mdi-pencil</v-icon>
+                <v-icon @click="deleteUser(user.id)">mdi-delete</v-icon>
+              </td>
+              <td v-else colspan="5">Brak akcji</td>
+            </tr>
+          </tbody>
+          <tbody v-else>
+            <tr>
+              <td colspan="5" class="text-center">Aktualnie tabela nie posiada żadnych rekordów.</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-data-table>
+    </v-card>
+
+    <v-dialog v-model="registerModalOpen">
+      <RegisterForm :editingMode="editingMode" :editedUserId="editedUserId" @close="registerModalOpen = false"
+        @register-success="handleRegisterSuccess" @user-updated="handleUserUpdated" ref="userForm" />
+    </v-dialog>
+  </v-container>
+</template>
+
+<script>
+import axios from 'axios';
+import RegisterForm from './Modals/RegisterForm.vue';
+
+export default {
+  data() {
+    return {
+      headers: [
+        { text: 'L.p.', value: 'id' },
+        { text: 'Nazwa', value: 'name' },
+        { text: 'Email', value: 'email' },
+        { text: 'Data dodania', value: 'created_at' },
+        { text: 'Akcje', value: 'actions', sortable: false },
+      ],
+      users: [],
+      registerModalOpen: false,
+      editedUserId: null,
+      editingMode: false
+    };
+  },
+  components: {
+    RegisterForm
+  },
+  created() {
+    this.fetchUsers();
+  },
+  methods: {
+    fetchUsers() {
+      axios.get('http://localhost:8000/api/users')
+        .then(response => {
+          this.users = response.data;
+        })
+        .catch(error => {
+          console.error('Błąd pobierania użytkowników:', error);
+        });
     },
-    created() {
+    editUser(user) {
+      this.editedUserId = user.id;
+      this.editingMode = true;
+      this.registerModalOpen = true;
+      this.$nextTick(() => {
+        if (this.$refs.userForm) {
+          this.$refs.userForm.editUser(user);
+        }
+      });
+    },
+    deleteUser(userId) {
+      axios.delete(`http://localhost:8000/api/users/${userId}`)
+        .then(() => {
+          this.users = this.users.filter(user => user.id !== userId);
+          window.alert('Usunięto użytkownika');
+        })
+        .catch(error => {
+          console.error('Błąd usuwania użytkownika:', error);
+        });
+    },
+    openRegisterModal() {
+      this.registerModalOpen = true;
+      this.editingMode = false;
+      this.$nextTick(() => {
+        if (this.$refs.userForm) {
+          this.$refs.userForm.resetForm();
+        }
+      });
+    },
+    handleRegisterSuccess(user) {
+      this.users.push(user);
+      this.registerModalOpen = false;
+    },
+    handleUserUpdated() {
       this.fetchUsers();
-    },
-    methods: {
-      fetchUsers() {
-        axios.get('http://localhost:8000/api/users')
-          .then(response => {
-            this.users = response.data;
-          })
-          .catch(error => {
-            console.error('Błąd pobierania użytkowników:', error);
-          });
-      },
-      editUser(user) {
-        console.log('Edytuj użytkownika:', user);
-      },
-      deleteUser(userId) {
-        axios.delete(`http://localhost:8000/api/users/${userId}`)
-          .then(() => {
-            this.users = this.users.filter(user => user.id !== userId);
-            window.alert('Usunięto użytkownika');
-          })
-          .catch(error => {
-            console.error('Błąd usuwania użytkownika:', error);
-          });
-      },
-    },
-  };
-  </script>
-  
-  <style>
-  @import '../styles/tables.scss'
-  </style>
-  
+      this.registerModalOpen = false;
+    }
+  }
+};
+</script>
+
+<style>
+@import '../styles/tables.scss';
+
+.inactive-row {
+  background-color: #f2f2f2;
+  opacity: 0.7;
+}
+</style>
---
+++ src/components/Modals/RegisterForm.vue
@@ -0,0 +1,111 @@
+<template>
+  <v-card>
+    <v-card-title v-if="!editingMode">Rejestracja</v-card-title>
+    <v-card-title v-else>Edytuj użytkownika</v-card-title>
+    <v-card-text>
+      <v-form @submit.prevent="register">
+        <v-text-field v-model="user.name" label="Nazwa"></v-text-field>
+        <v-text-field v-model="user.email" label="Email" type="email"></v-text-field>
+        <v-text-field v-model="user.password" label="Hasło (min. 6 znaków)"
+          :append-icon="user.showPassword ? 'mdi-eye' : 'mdi-eye-off'" :type="user.showPassword ? 'text' : 'password'"
+          @click:append="user.showPassword = !user.showPassword"></v-text-field>
+        <v-text-field v-model="user.confirmPassword" label="Potwierdź hasło"
+          :append-icon="user.showConfirmPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          :type="user.showConfirmPassword ? 'text' : 'password'"
+          @click:append="user.showConfirmPassword = !user.showConfirmPassword"></v-text-field>
+        <v-card-actions>
+          <v-btn color="grey darken-3" dark type="submit">{{ editingMode ? 'Zapisz zmiany' : 'Zarejestruj użytkownika'
+            }}</v-btn>
+        </v-card-actions>
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  props: {
+    editingMode: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      user: {
+        id: null,
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        showPassword: false,
+        showConfirmPassword: false,
+      },
+    };
+  },
+  methods: {
+    register() {
+      if (this.user.password.length < 6) {
+        window.alert('Hasło musi zawierać co najmniej 6 znaków.');
+        return;
+      }
+
+      if (this.user.password !== this.user.confirmPassword) {
+        window.alert('Hasła się nie zgadzają.');
+        return;
+      }
+
+      const formData = {
+        name: this.user.name,
+        email: this.user.email,
+        password: this.user.password,
+      };
+
+      if (this.editingMode) {
+        axios.put(`http://localhost:8000/api/users/${this.user.id}`, formData)
+          .then(response => {
+            window.alert('Użytkownik został zaktualizowany pomyślnie');
+            this.$emit('user-updated', response.data);
+            this.resetForm();
+          })
+          .catch(error => {
+            console.error('Błąd podczas aktualizacji użytkownika:', error);
+            window.alert('Błąd podczas aktualizacji użytkownika');
+          });
+      } else {
+        axios.post('http://localhost:8000/api/register', formData)
+          .then(response => {
+            const user = response.data.user;
+            this.$emit('register-success', user);
+            this.resetForm();
+            window.alert('Rejestracja przebiegła pomyślnie.');
+          })
+          .catch(error => {
+            console.error('Błąd rejestracji:', error);
+            window.alert('Błąd rejestracji. Sprawdź swoje dane.');
+          });
+      }
+    },
+    resetForm() {
+      this.user.id = null;
+      this.user.name = '';
+      this.user.email = '';
+      this.user.password = '';
+      this.user.confirmPassword = '';
+      this.user.showPassword = false;
+      this.user.showConfirmPassword = false;
+    },
+    editUser(user) {
+      this.user.id = user.id;
+      this.user.name = user.name;
+      this.user.email = user.email;
+    }
+  }
+};
+</script>
+
+<style>
+@import '../../styles/forms.scss';
+</style>
